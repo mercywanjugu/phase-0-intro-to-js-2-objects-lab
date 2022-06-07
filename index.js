980b3a80131c7af0c24b69c39a3aad8ba3427be1
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-07 02:44:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+  };
+  const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return { ...employee, [key]: value };
+  };
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  
+  const deleteFromEmployeeByKey = (employee, key) => {
+      let newEmployee = {...employee}
+      delete newEmployee[key];
+    
+      return newEmployee;
+  }
+  
+  
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+      delete employee[key];
+      return employee;
+  }
